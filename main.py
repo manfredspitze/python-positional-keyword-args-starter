35673733266ea264ca656d3e44a68e7df68bc994
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-11-13 05:21:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# Student name
+# Current date
+# Practice: Positional & Keyword Arguments in Python
+
+
+# POSITIONAL ARGUMENTS
+# Start by writing the code to create the three functions outlined below
+# You'll use only positional arguments in the first three functions
+# Test your code and correct any errors
+
+# Greet The User
+# Write a function that takes two parameters -- first name and age
+# Use an f-string to welcome the user by first name and to display his/her age
+
+
+# Area of a Rectangle
+# Write a function to calculate the area (in square feet) of a rectangle
+# Your two parameters will be length and width
+# The print statement in the function should display the length, width and area (in square feet) of your rectangle
+
+# Sum of Numbers
+# Write a function that finds the sum of three numbers
+# Your three parameters will be num1, num2, and num3
+# The print statement in the function should display the sum of the three numbers
+
+
+
+# KEYWORD ARGUMENTS
+# Create the three functions outlined below
+# In these last three functions, you'll use only keyword arguments 
+# Test your code and correct any errors
+
+# Greet By Title
+# Write a function that greets the user by title, first name and last name
+# Examples of titles include: Mr., Ms., Mrs. and Dr.
+# When calling your function, change the order of your keyword arguments so that they don't match the order of your function parameters
+
+# Describe Your Pet
+# Write a function that says what type of pet you have and what your pet's name is
+# Your function parameters will be pet_type and pet_name
+
+# Updated Function
+# Choose any ONE of the first three functions from this project and rewrite it below using keyword arguments
